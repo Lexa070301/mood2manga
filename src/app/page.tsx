676546da2040f714a3cfa151dddb152d3559bccb
Alexey <lexa070301@gmail.com>
2024-07-18 23:00:30 +0400

    add analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import styles from "./page.module.css";
 import Emotions from "@/components/Emotions/Emotions";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Emotions />
+      <Script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="1603b607-4e13-402f-9bdb-d96e4ae689bc"
+      />
     </main>
   );
 }
