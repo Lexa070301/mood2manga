e01769225be3253f37b1757a2e340b5785ec3188
Alexey <lexa070301@gmail.com>
2024-07-23 12:47:40 +0400

    add openGraph meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,13 @@ export const metadata: Metadata = {
   title: "Mood2Manga",
   description:
     "Discover the perfect manga for your mood! Our website helps you find manga tailored to your feelings, whether you're looking for something uplifting, thrilling, or heartwarming. Explore curated recommendations and dive into stories that match your emotions. Find your next favorite read today!",
+  openGraph: {
+    title: "Mood2Manga - Find the perfect manga for your mood",
+    description: "Stop spending hours searching for manga to read. Let your emotions make the choice for you",
+    url: "https://mood2manga.vercel.app",
+    siteName: "Mood2Manga",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
